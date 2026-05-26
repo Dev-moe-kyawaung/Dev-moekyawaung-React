fad7f022255a341669560594c194ea4034b8a100
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-05-27 02:46:22 +0630

    Create ProjectsPage.jsx

---
+++ src/pages/ProjectsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Projects from "../components/sections/Projects";
+import Certificates from "../components/sections/Certificates";
+
+export default function ProjectsPage() {
+  return (
+    <>
+      <Projects />
+      <Certificates />
+    </>
+  );
+}
