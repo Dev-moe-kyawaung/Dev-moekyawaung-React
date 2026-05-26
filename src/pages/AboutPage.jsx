6d9120ea28008e23cc83dabd466baadeac6461fd
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-05-27 02:46:44 +0630

    Create AboutPage.jsx

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import About from "../components/sections/About";
+import Skills from "../components/sections/Skills";
+
+export default function AboutPage() {
+  return (
+    <>
+      <About />
+      <Skills />
+    </>
+  );
+}
