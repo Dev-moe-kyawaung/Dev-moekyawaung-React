2f6a3aedd87467e99a8bc315f3f353fd395cb9e1
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-05-27 02:45:57 +0630

    Create ServicesPage.jsx

---
+++ src/pages/ServicesPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Pricing from "../components/sections/Pricing";
+import Testimonials from "../components/sections/Testimonials";
+import FAQ from "../components/sections/FAQ";
+
+export default function ServicesPage() {
+  return (
+    <>
+      <Pricing />
+      <Testimonials />
+      <FAQ />
+    </>
+  );
+}
